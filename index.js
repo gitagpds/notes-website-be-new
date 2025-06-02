22de8b67718f7acb46f79f91e7c55725e79677f6
Gita Poetri Dewi Siregar <146951904+gitagpds@users.noreply.github.com>
2025-06-02 11:06:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000", // <- Diganti sama alamat front-end
+    origin: "http://localhost:5173", // <- Diganti sama alamat front-end
     credentials: true,
   })
 );
